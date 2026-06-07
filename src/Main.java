f57cbbc0b2cd872f0f5c1dfa0a62347c4e4b00ca
Valentin Herrmann <accounts@valentin-herrmann.com>
2026-06-07 23:39:39 +0200

    finished solution

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,9 @@
 /**
-Erstelle dein Programm über Blockly und
-klicke auf 'Play', um es auszuführen!
-*/
+ * Erstelle dein Programm über Blockly und
+ * klicke auf 'Play', um es auszuführen!
+ */
 
-public class Main { 
+public class Main {
     /**
      * Das Hauptprogramm (main-Methode):
      * Alles, was ausgeführt werden soll,
@@ -14,6 +14,53 @@ public class Main {
      * gestartet, wenn du auf 'Play' drückst.
      */
     public static void main() {
+        System.out.println(aufgabe1());
+    }
+
+    public static int[] aufgabe1() {
+        int[] arr1 = {1, 2, 3};
+        return arr1;
+    }
+
+    public static double[] aufgabe2() {
+        double[] arr2 = new double[11];
+        for (int i = 0; i < 11; i++) {
+            arr2[i] = Math.pow(2, i);
+        }
+        return arr2;
+    }
+
+    public static Object aufgabe3(String[] arr) {
+        return arr[0];
+    }
+
+    public static Object aufgabe4(int[] arr) {
+        return arr[arr.length - 1];
+    }
+
+    public static void aufgabe5(String[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println(arr[i]);
+        }
+    }
+
+    public static int[] aufgabe6(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = 2 * arr[0];
+        }
+        return arr;
+    }
+
+    public static int aufgabe7(int[] arr) {
+        int summe = 0;
+        for (int i = 0; i < arr.length; i++) {
+            summe = summe + arr[i];
+        }
+        return summe;
     }
 
 }
+
+
+// main()-Methode starten
+Main.main();
